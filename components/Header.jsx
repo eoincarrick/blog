85c331f2a347659e09808f20894e272d7d30818a
Eoin Carrick <22carri99@gmail.com>
2022-03-08 21:43:46 +0000

    saave

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,23 +9,27 @@ const Header = () => {
   const handleClick = () => {
     setClicked(!clicked);
   };
-
+  console.log(menu);
   return (
-    <div className="flex justify-center justify-between bg-black text-white px-5 w-full">
-      <div id="logo__container">CharInfo</div>
+    <div className="flex justify-between bg-black text-white px-5 h-10 w-full ">
+      <div id="logo__container" className="py-2 m-1">
+        <Link href={menu[0].url}>
+          <span className="mx-5 hover:cursor-pointer m-1 rounded-lg border-l-2 border-r-2   ">
+            CharInfo
+          </span>
+        </Link>
+      </div>
 
       <div id="menuList__container">
-        <ul className="flex justify-around">
+        <ul className="flex justify-around w-full ">
           {menu.map((item, index) => (
-            <Link className="mx-8" href={item.url}>
-              {item.title}
+            <Link key={index} href={item.url}>
+              <span className="mx-5 hover:cursor-pointer p-1 m-1 rounded-lg hover:bg-white hover:text-black ease-in-out duration-500 md:gr">
+                {item.title}
+              </span>
             </Link>
           ))}
         </ul>
-      </div>
-
-      <div onClick={handleClick}>
-        <span>{clicked ? <FaTimes /> : <FaBars />}</span>
       </div>
     </div>
   );
