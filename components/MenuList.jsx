30751f3a180929cf81440fb03fcf4303b3b66ebe
Eoin Carrick <22carri99@gmail.com>
2022-03-08 09:25:24 +0000

    code

---
+++ components/MenuList.jsx
@@ -0,0 +1,30 @@
+export const MenuList = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/About",
+  },
+  {
+    title: "Plan",
+    url: "/Plan",
+  },
+  {
+    title: "Features",
+    url: "/Features",
+  },
+  {
+    title: "Demo",
+    url: "/Demo",
+  },
+  {
+    title: "Contact",
+    url: "/Contact",
+  },
+  {
+    title: "SignUp",
+    url: "/SignUp",
+  },
+];
