205bcdab7dc5aad616b7bf1ac3151eac4e9f72a4
Eoin Carrick <22carri99@gmail.com>
2022-03-28 14:06:17 +0000

    cdoe

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import css from '../styles/Header.module.css';
-import { FaTimes, FaBars } from 'react-icons/fa';
+import { FaTimes, FaBars, FaAngleDown, FaAngleUp } from 'react-icons/fa';
 
 const Header = () => {
-  const [clicked, setClicked] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showCategory, setShowCategory] = useState(false);
 
-  const handleClick = () => {
-    setClicked(!clicked);
+  const handleClickMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleClickCategory = () => {
+    setShowCategory(!showCategory);
   };
 
   return (
@@ -20,78 +25,80 @@ const Header = () => {
           </div>
         </div>
 
-        <div className={css.navContainer}>
+        <div
+          className={`${showMenu ? css.navContainer : css.navContainerClose}`}
+        >
           <ul className={css.navItems}>
             <Link href='/'>
-              <li onClick={handleClick} className={`${css.item}`}>
+              <li onClick={handleClickMenu} className={`${css.item}`}>
                 Home
               </li>
             </Link>
-            <Link href='/#'>
-              <div>
-                <li onClick={handleClick} className={`${css.item} `}>
-                  Categories
+
+            <div>
+              <li onClick={handleClickCategory} className={`${css.item} `}>
+                Category{' '}
+                {showCategory ? (
+                  <FaAngleUp className={css.icon} />
+                ) : (
+                  <FaAngleDown className={css.icon} />
+                )}
+              </li>
+              <div className={showCategory ? css.category : css.categoryClose}>
+                <li onClick={handleClickMenu} className={css.listType}>
+                  <Image src='/css.png' alt='css' width='30px' height='30px' />{' '}
+                  <Link href={`/category/css`}>CSS</Link>
                 </li>
-                <div className={clicked ? css.category : css.categoryClose}>
-                  <li onClick={handleClick} className={css.listType}>
-                    <Image
-                      src='/css.png'
-                      alt='css'
-                      width='30px'
-                      height='30px'
-                    />{' '}
-                    CSS
-                  </li>
-                  <li onClick={handleClick} className={css.listType}>
-                    <Image
-                      src='/html.png'
-                      alt='html'
-                      width='30px'
-                      height='30px'
-                    />{' '}
-                    HTML
-                  </li>
-                  <li onClick={handleClick} className={css.listType}>
-                    <Image
-                      src='/js.png'
-                      alt='javascript'
-                      width='30px'
-                      height='30px'
-                    />{' '}
-                    JavaScript
-                  </li>
-                  <li onClick={handleClick} className={css.listType}>
-                    <Image
-                      src='/nodejs.png'
-                      alt='nodejs'
-                      width='30px'
-                      height='30px'
-                    />
-                    {'  '}
-                    NodeJS
-                  </li>
-                  <li onClick={handleClick} className={css.listType}>
-                    <Image
-                      src='/python.png'
-                      alt='python'
-                      width='30px'
-                      height='30px'
-                    />
-                    {'  '}
-                    Python
-                  </li>
-                </div>
+                <li onClick={handleClickMenu} className={css.listType}>
+                  <Image
+                    src='/html.png'
+                    alt='html'
+                    width='30px'
+                    height='30px'
+                  />{' '}
+                  <Link href={`/category/html`}>HTML</Link>
+                </li>
+                <li onClick={handleClickMenu} className={css.listType}>
+                  <Image
+                    src='/js.png'
+                    alt='javascript'
+                    width='30px'
+                    height='30px'
+                  />{' '}
+                  <Link href={`/category/javascript`}>JavaScript</Link>
+                </li>
+                <li onClick={handleClickMenu} className={css.listType}>
+                  <Image
+                    src='/nodejs.png'
+                    alt='nodejs'
+                    width='30px'
+                    height='30px'
+                  />
+                  {'  '}
+                  <Link href={`/category/nodejs`}>NodeJS</Link>
+                </li>
+                <li onClick={handleClickMenu} className={css.listType}>
+                  <Image
+                    src='/python.png'
+                    alt='python'
+                    width='30px'
+                    height='30px'
+                  />
+                  {'  '}
+                  <Link href={`/category/python`}>Python</Link>
+                </li>
               </div>
-            </Link>
+            </div>
+
             <Link href='/create-account'>
               <li className={`${css.item}`}>Create Account</li>
             </Link>
           </ul>
         </div>
 
-        <div onClick={handleClick} className={css.buttonContainer}>
-          {clicked ? (
-            <FaTimes className={css.icons} />
+        <div onClick={handleClickMenu} className={css.buttonContainer}>
+          {showMenu ? (
+            <FaTimes className={css.iconsTimes} />
           ) : (
             <FaBars className={css.icons} />
           )}
---
+++ pages/category/[slug].js
@@ -0,0 +1,8 @@
+//React imports
+import React from 'react';
+
+const Category = () => {
+  return <h1>Hello World!</h1>;
+};
+
+export default Category;
